243b29911f9e759b78205aeb2344cff247505f9a
antoniotarricone <antonio.tarricone@pagopa.it>
2023-03-31 09:42:14 +0200

    Code review.

---
+++ src/main/java/it/gov/pagopa/swclient/mil/idp/validation/constraints/GrantType.java
@@ -0,0 +1,32 @@
+/*
+ * GrantType.java
+ *
+ * 28 mar 2023
+ */
+package it.gov.pagopa.swclient.mil.idp.validation.constraints;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ * If grant_type = password, username and password must not be null and refresh_token must be null.
+ * If grant_type = refresh_token, refresh token must not be null and username and password must be null.
+ * 
+ * @author Antonio Tarricone
+ */
+@Documented
+@Retention(RUNTIME)
+@Target(TYPE)
+@Constraint(validatedBy = { GrantTypeValidator.class })
+public @interface GrantType {
+	String message() default "";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/it/gov/pagopa/swclient/mil/idp/validation/constraints/GrantTypeValidator.java
@@ -0,0 +1,30 @@
+/*
+ * GrantTypeValidator.java
+ *
+ * 28 mar 2023
+ */
+package it.gov.pagopa.swclient.mil.idp.validation.constraints;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import it.gov.pagopa.swclient.mil.idp.bean.GetAccessToken;
+
+/**
+ * If grant_type = password, username and password must not be null and refresh_token must be null.
+ * If grant_type = refresh_token, refresh token must not be null and username and password must be null.
+ * 
+ * @author Antonio Tarricone
+ */
+public class GrantTypeValidator implements ConstraintValidator<GrantType, GetAccessToken> {
+	@Override
+	public boolean isValid(GetAccessToken getAccessToken, ConstraintValidatorContext context) {
+		boolean isValid = false;
+		if (getAccessToken.getGrantType().equals("password")) {
+			isValid = (getAccessToken.getUsername() != null && getAccessToken.getPassword() != null && getAccessToken.getRefreshToken() == null);
+		} else {
+			isValid = (getAccessToken.getUsername() == null && getAccessToken.getPassword() == null && getAccessToken.getRefreshToken() != null);
+		}
+		return isValid;
+	}
+}
