4875bf6a7320b59e98622f61c7de1a985fc04185
antoniotarricone <antonio.tarricone@pagopa.it>
2023-04-04 09:53:34 +0200

    Added @RegisterForReflection.

--- src/main/java/it/gov/pagopa/swclient/mil/idp/bean/PublicKey.java
+++ src/main/java/it/gov/pagopa/swclient/mil/idp/bean/PublicKey.java
@@ -7,10 +7,13 @@ package it.gov.pagopa.swclient.mil.idp.bean;
 
 import java.util.Objects;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 /**
  * 
  * @author Antonio Tarricone
  */
+@RegisterForReflection
 public class PublicKey {
 	/*
 	 * Public exponent
