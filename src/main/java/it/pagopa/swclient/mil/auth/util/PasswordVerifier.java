1d88f4462d1cf88a09b9b43fdea30c1838c72149
antoniotarricone <antonio.tarricone@pagopa.it>
2023-05-19 17:27:41 +0200

    Extracted the logic to calculate the hash.

--- src/main/java/it/pagopa/swclient/mil/auth/util/PasswordVerifier.java
+++ src/main/java/it/pagopa/swclient/mil/auth/util/PasswordVerifier.java
@@ -31,16 +31,41 @@ public class PasswordVerifier {
 	 * @throws NoSuchAlgorithmException
 	 */
 	public static boolean verify(String password, String salt, String hash) throws NoSuchAlgorithmException {
+		byte[] hashBytes = Base64.getDecoder().decode(hash);
+		byte[] calcHashBytes = hashBytes(password, salt);
+		return Arrays.equals(calcHashBytes, hashBytes);
+	}
+	
+	/**
+	 * 
+	 * @param password
+	 * @param salt
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	private static byte[] hashBytes(String password, String salt) throws NoSuchAlgorithmException {
 		byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
 		byte[] saltBytes = Base64.getDecoder().decode(salt);
-		byte[] hashBytes = Base64.getDecoder().decode(hash);
-
+		
 		byte[] data = new byte[passwordBytes.length + saltBytes.length];
 		System.arraycopy(passwordBytes, 0, data, 0, passwordBytes.length);
 		System.arraycopy(saltBytes, 0, data, passwordBytes.length, saltBytes.length);
-
+		
 		MessageDigest digest = MessageDigest.getInstance("SHA256");
-		byte[] calcHashBytes = digest.digest(data);
-		return Arrays.equals(calcHashBytes, hashBytes);
+		byte[] hashBytes = digest.digest(data);
+		return hashBytes;
+	}
+	
+	/**
+	 * 
+	 * @param password
+	 * @param salt
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static String hash(String password, String salt) throws NoSuchAlgorithmException {
+		byte[] hashBytes = hashBytes(password, salt);
+		String hash = Base64.getEncoder().encodeToString(hashBytes);
+		return hash;
 	}
 }
