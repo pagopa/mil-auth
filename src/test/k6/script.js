45efe36e8290b3ec16279b7c8c561d286a9dca99
Antonio Tarricone <110115827+antoniotarricone@users.noreply.github.com>
2023-06-07 15:31:44 +0200

    Update script.js

--- src/test/k6/script.js
+++ src/test/k6/script.js
@@ -3,11 +3,11 @@ import { sleep, check } from 'k6';
 
 export const options = {
 	maxRedirects: 4,
-	duration: '10s',
-	vus: 125,
+	duration: '5m',
+	vus: 100,
 	thresholds: {
 		// 90% of requests must finish within 4s, 95% within 5s.
-		'http_req_duration': ['p(90) < 2500', 'p(95) < 3000'],
+		'http_req_duration': ['p(90) < 3000', 'p(95) < 5000'],
 		// During the whole test execution, the error rate (HTTP status 500) must be lower than 1%.
 		'checks{myTag:serverError}': ['rate < 0.01'],
 	}
