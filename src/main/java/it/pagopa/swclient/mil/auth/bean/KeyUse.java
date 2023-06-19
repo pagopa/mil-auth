8c014ada0ea7960a8c84614cc98786fd76199067
Antonio Tarricone <110115827+antoniotarricone@users.noreply.github.com>
2023-06-19 19:45:07 +0200

    fix: Removed useless complexity. (#50)

--- src/main/java/it/pagopa/swclient/mil/auth/bean/KeyUse.java
+++ src/main/java/it/pagopa/swclient/mil/auth/bean/KeyUse.java
@@ -5,57 +5,10 @@
  */
 package it.pagopa.swclient.mil.auth.bean;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 /**
  * 
  * @author Antonio Tarricone
  */
 public enum KeyUse {
-	SIG("sig");
-
-	/*
-	 * String value.
-	 */
-	private final String string;
-
-	/**
-	 * 
-	 * @param string
-	 */
-	private KeyUse(String string) {
-		this.string = string;
-	}
-
-	/**
-	 * 
-	 */
-	@JsonValue
-	@Override
-	public String toString() {
-		return string;
-	}
-
-	/**
-	 * 
-	 * @param string
-	 * @return
-	 */
-	@JsonCreator
-	public static KeyUse fromString(String string) {
-		if (string == null) {
-			throw new IllegalArgumentException("value is null");
-		}
-
-		int i = 0;
-		while (i < values().length && !values()[i].toString().equals(string)) {
-			i++;
-		}
-		if (values()[i].toString().equals(string)) {
-			return values()[i];
-		}
-
-		throw new IllegalArgumentException(String.format("%s doesn't exist", string));
-	}
+	SIG
 }
