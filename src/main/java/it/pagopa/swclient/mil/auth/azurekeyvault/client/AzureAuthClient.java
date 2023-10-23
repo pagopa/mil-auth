cb7adc47f0c776c701722a94dbd1c73ffa430a5b
antoniotarricone <antonio.tarricone@pagopa.it>
2023-10-23 10:57:25 +0200

    Fixed the identity endpoint.

--- src/main/java/it/pagopa/swclient/mil/auth/azurekeyvault/client/AzureAuthClient.java
+++ src/main/java/it/pagopa/swclient/mil/auth/azurekeyvault/client/AzureAuthClient.java
@@ -7,13 +7,13 @@ package it.pagopa.swclient.mil.auth.azurekeyvault.client;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import io.quarkus.rest.client.reactive.ClientQueryParam;
 import io.smallrye.mutiny.Uni;
 import it.pagopa.swclient.mil.auth.azurekeyvault.bean.GetAccessTokenResponse;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HeaderParam;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 /**
@@ -22,17 +22,14 @@ import jakarta.ws.rs.core.MediaType;
 @RegisterRestClient(configKey = "azure-auth-api")
 public interface AzureAuthClient {
 	/**
-	 * @param tenantId
-	 * @param grantType
-	 * @param clientId
-	 * @param clientSecret
+	 * @param identity
 	 * @param scope
 	 * @return
 	 */
-	@Path("?resource={scope}&api-version=2019-08-01")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@ClientQueryParam(name = "api-version", value = "${azure-auth-api.version}")
 	Uni<GetAccessTokenResponse> getAccessToken(
 		@HeaderParam("x-identity-header") String identity,
-		@PathParam("scope") String scope);
+		@QueryParam("resource") String scope);
 }
