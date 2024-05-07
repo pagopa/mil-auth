34bf3c64e84f378e2d2601931caf3befa6a647b1
antoniotarricone <antonio.tarricone@pagopa.it>
2024-05-07 09:55:10 +0200

    Access token cache removed.
    Due to insufficiently robust management of the access token cache, this
    has been removed.

--- src/main/java/it/pagopa/swclient/mil/auth/azure/auth/service/AzureAuthService.java
+++ src/main/java/it/pagopa/swclient/mil/auth/azure/auth/service/AzureAuthService.java
@@ -5,10 +5,6 @@
  */
 package it.pagopa.swclient.mil.auth.azure.auth.service;
 
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -16,7 +12,6 @@ import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
 import it.pagopa.swclient.mil.auth.azure.auth.bean.GetAccessTokenResponse;
 import it.pagopa.swclient.mil.auth.azure.auth.client.AzureAuthClient;
-import it.pagopa.swclient.mil.auth.util.UniGenerator;
 import jakarta.enterprise.context.ApplicationScoped;
 
 /**
@@ -41,30 +36,16 @@ public class AzureAuthService {
 	 */
 	private String identity;
 
-	/*
-	 * 
-	 */
-	private boolean cacheAccessTokens;
-
-	/*
-	 * 
-	 */
-	private Map<String, GetAccessTokenResponse> accessTokens;
-
 	/**
 	 * 
 	 * @param identity
-	 * @param cacheAccessTokens
 	 * @param client
 	 */
 	AzureAuthService(
 		@ConfigProperty(name = "azure-auth-api.identity") String identity,
-		@ConfigProperty(name = "azure-auth-api.cache-access-tokens") boolean cacheAccessTokens,
 		@RestClient AzureAuthClient client) {
 		this.identity = identity;
-		this.cacheAccessTokens = cacheAccessTokens;
 		this.client = client;
-		accessTokens = new HashMap<>();
 	}
 
 	/**
@@ -72,15 +53,8 @@ public class AzureAuthService {
 	 * @return
 	 */
 	public Uni<GetAccessTokenResponse> getAccessToken() {
-		GetAccessTokenResponse accessToken = accessTokens.get(VAULT);
-		if (cacheAccessTokens && accessToken != null && accessToken.getExpiresOn() > Instant.now().getEpochSecond()) {
-			Log.debug("Access token for Key Vault already got.");
-			return UniGenerator.item(accessToken);
-		} else {
-			Log.debug("Authenticating to Azure AD for Key Vault.");
-			return client.getAccessToken(identity, VAULT)
-				.invoke(i -> accessTokens.put(VAULT, i));
-		}
+		Log.debug("Authenticating to Azure AD for Key Vault.");
+		return client.getAccessToken(identity, VAULT);
 	}
 
 	/**
@@ -88,14 +62,7 @@ public class AzureAuthService {
 	 * @return
 	 */
 	public Uni<GetAccessTokenResponse> getAccessTokenForStorage() {
-		GetAccessTokenResponse accessToken = accessTokens.get(STORAGE);
-		if (cacheAccessTokens && accessToken != null && accessToken.getExpiresOn() > Instant.now().getEpochSecond()) {
-			Log.debug("Access token for Storage Account already got.");
-			return UniGenerator.item(accessToken);
-		} else {
-			Log.debug("Authenticating to Azure AD for Storage Account.");
-			return client.getAccessToken(identity, STORAGE)
-				.invoke(i -> accessTokens.put(STORAGE, i));
-		}
+		Log.debug("Authenticating to Azure AD for Storage Account.");
+		return client.getAccessToken(identity, STORAGE);
 	}
 }
