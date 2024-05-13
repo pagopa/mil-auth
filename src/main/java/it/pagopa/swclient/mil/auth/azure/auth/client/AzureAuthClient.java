badec5dff0cda4cd7fa3eb48bb22d9f9c8788f82
Antonio Tarricone <110115827+antoniotarricone@users.noreply.github.com>
2024-05-13 08:36:38 +0200

    chore: Removed unnecessary spans. (#81)

--- src/main/java/it/pagopa/swclient/mil/auth/azure/auth/client/AzureAuthClient.java
+++ src/main/java/it/pagopa/swclient/mil/auth/azure/auth/client/AzureAuthClient.java
@@ -7,8 +7,6 @@ package it.pagopa.swclient.mil.auth.azure.auth.client;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.quarkus.rest.client.reactive.ClientQueryParam;
 import io.smallrye.mutiny.Uni;
 import it.pagopa.swclient.mil.auth.azure.auth.bean.GetAccessTokenResponse;
@@ -31,7 +29,6 @@ public interface AzureAuthClient {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@ClientQueryParam(name = "api-version", value = "${azure-auth-api.version}")
-	@WithSpan(kind = SpanKind.CLIENT)
 	Uni<GetAccessTokenResponse> getAccessToken(
 		@HeaderParam("x-identity-header") String identity,
 		@QueryParam("resource") String scope);
